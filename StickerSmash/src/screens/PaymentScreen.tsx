8d28085169936b87cf215168e8af50a2ac54a2df
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-01-09 17:27:22 +0100

    add svg component

--- StickerSmash/src/screens/PaymentScreen.tsx
+++ StickerSmash/src/screens/PaymentScreen.tsx
@@ -1,3 +1,14 @@
 import React from "react";
 import {Image, View, Button, Text, TouchableOpacity, StyleSheet, TextInput, SafeAreaView} from 'react-native';
 import SvgComponent from "../components/CardSVG";
+
+
+export default function PaymentScreen(){
+    return (
+        <SafeAreaView>
+            <Text></Text>
+            <Image></Image>
+            
+        </SafeAreaView>
+    )
+}
