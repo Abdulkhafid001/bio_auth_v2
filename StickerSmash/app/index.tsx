7c9a1285e1b094f6c5c841c427f4c08bc863d559
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-01-09 11:47:27 +0100

    add js files config

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen. this is a screen lol</Text>
     </View>
   );
 }
